89fd387f86361597064a70ffbe8ab28f6dea5464
Diary1213 <cmp1213@naver.com>
2022-08-02 22:03:59 +0900

    make house!

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.rect(50, 50, 100, 100);
-ctx.rect(150, 150, 100, 100);
-ctx.rect(250, 250, 100, 100);
-ctx.fill();
+ctx.fillRect(200, 200, 50, 200);
+ctx.fillRect(400, 200, 50, 200);
 
-ctx.beginPath();
-ctx.rect(350, 350, 100, 100);
-ctx.fillStyle = "red";
+ctx.fillRect(300, 300, 50, 100);
+ctx.fillRect(200, 200, 200, 20);
+ctx.moveTo(200, 200);
+ctx.lineTo(325, 150);
+ctx.lineTo(450, 200);
 ctx.fill();
